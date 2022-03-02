96e44747b3ac924a3dd4345e815d04d8ab83a6a1
Kenbayashi <kenbayashi.gaming.00@gmail.com>
2022-03-02 17:31:37 +0900

    b

--- src/bin/b.rs
+++ src/bin/b.rs
@@ -1,3 +1,31 @@
+use std::collections::HashMap;
+
 fn main() {
-    unimplemented!();
+    proconio::input! {
+        n: usize,
+        m: usize,
+        a: [u32; n],
+        b: [u32; m],
+    }
+
+    let mut result = true;
+    let mut map = HashMap::<u32, u32>::new();
+
+    for num in a {
+        *map.entry(num).or_default() += 1;
+    }
+
+    for num in b {
+        if let Some(&count) = map.get(&num) {
+            if 1 <= count {
+                map.insert(num, count - 1);
+            } else {
+                result = false;
+            }
+        } else {
+            result = false;
+        }
+    }
+
+    println!("{}", if result {"Yes"} else {"No"});
 }
